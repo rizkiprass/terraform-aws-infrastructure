8471451873bcc83314546228f674e5f4c2a93f24
rizkiprass <rizkiprass@gmail.com>
2023-08-21 21:18:20 +0700

    add security group

--- simple-ec2/ec2.tf
+++ simple-ec2/ec2.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "web" {
-  ami           = "ami-07e67bd6b5d9fd892"
-  instance_type = "t3.micro"
+  ami                    = "ami-008bcc0a51a849165" #ubuntu 20
+  instance_type          = "t3.micro"
+  key_name               = "react-ec2-key"
+  vpc_security_group_ids = [aws_security_group.security_group_react.id]
 
   tags = {
-    Name = "react-app-ec2"
+    Name = "react-app-ec2" #give your ec2 name here
   }
 }
--- simple-ec2/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {
-  description = "AWS Region"
-  default     = "eu-west-3"
-}
-
-#variable "access_key" {}
-#
-#variable "secret_key" {}
